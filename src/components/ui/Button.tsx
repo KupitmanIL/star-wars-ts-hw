15b5af0128af3571d3d7d72614aca38d1deae942
edward <edwardlfish@gmail.com>
2026-03-05 20:58:28 +0200

    Add initial Button and Text component placeholders

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,11 @@
+const Button = () => {
+    return (
+        <div>
+            {/*
+                TODO create ui for Button and use it in NavItem and Footer
+            */}
+        </div>
+    )
+}
+
+export default Button;
---
+++ src/components/ui/Text.tsx
@@ -0,0 +1,11 @@
+const Text = () => {
+    return (
+        <div>
+            {/*
+                TODO create ui for Text and use it in StarWars and OpeningCrawl
+            */}
+        </div>
+    )
+}
+
+export default Text;
