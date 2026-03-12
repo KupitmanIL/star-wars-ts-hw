8cc2435963480f9006515fadd2bbdfcaec4e6fee
Ilia Kupitman <KupitmanIL@vk.com>
2026-03-12 21:04:08 +0200

    12.03

---
+++ src/utils/types.d.ts
@@ -0,0 +1,11 @@
+export interface SWConrextValue {
+    hero: string;
+    changeHero: (hero: string) => void;
+}
+
+export interface Hero {
+    name: string;
+    img: string;
+    url: string;
+}
+
