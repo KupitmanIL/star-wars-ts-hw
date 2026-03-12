ea24d2365cc14c2bde2007b6a0de3e3052ca4452
Ilia Kupitman <KupitmanIL@vk.com>
2026-03-12 22:13:21 +0200

    12.03 HW

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,4 +1,8 @@
-import {characters} from "../utils/constants.ts";
+import {characters, defaultHero} from "../utils/constants.ts";
+import {NavLink, useParams} from "react-router";
+import {useContext, useEffect} from "react";
+import {SWContext} from "../utils/context.ts";
+import ErrorPage from "./ErrorPage.tsx";
 
 interface Props {
     friend: string;
@@ -6,6 +10,16 @@ interface Props {
 }
 
 const Friend = ({friend, pos}: Props) => {
+    const {changeHero} = useContext(SWContext)
+    const {heroId = defaultHero} = useParams()
+
+    useEffect(() => {
+        if (!(heroId in characters)) {
+            return;
+        }
+        changeHero(heroId);
+    }, [heroId]);
+
     let styles = "w-full";
     if (pos === 9) {
         styles += " rounded-br-3xl";
@@ -13,9 +27,11 @@ const Friend = ({friend, pos}: Props) => {
     if (pos === 7) {
         styles += " rounded-bl-3xl";
     }
-    return (
+    return  (heroId in characters) ? (
+    <NavLink to={`/home/${friend}`}>
         <img className={styles} src={characters[friend].img} alt={characters[friend].name}/>
-    )
+    </NavLink>
+    ) : <ErrorPage/>
 }
 
 export default Friend;
