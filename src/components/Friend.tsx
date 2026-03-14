1ddcb2cb56603c43c870ba0daa70cb0383d9b4fa
Ilia Kupitman <KupitmanIL@vk.com>
2026-03-14 12:56:11 +0200

    14.03 HW

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,8 +1,8 @@
-import {characters, defaultHero} from "../utils/constants.ts";
-import {NavLink, useParams} from "react-router";
-import {useContext, useEffect} from "react";
+import {characters} from "../utils/constants.ts";
+import {NavLink} from "react-router";
+import {useContext} from "react";
 import {SWContext} from "../utils/context.ts";
-import ErrorPage from "./ErrorPage.tsx";
+
 
 interface Props {
     friend: string;
@@ -11,26 +11,16 @@ interface Props {
 
 const Friend = ({friend, pos}: Props) => {
     const {changeHero} = useContext(SWContext)
-    const {heroId = defaultHero} = useParams()
-
-    useEffect(() => {
-        if (!(heroId in characters)) {
-            return;
-        }
-        changeHero(heroId);
-    }, [heroId]);
 
     let styles = "w-full";
-    if (pos === 9) {
-        styles += " rounded-br-3xl";
-    }
-    if (pos === 7) {
-        styles += " rounded-bl-3xl";
-    }
+    if (pos === 9) styles += " rounded-br-3xl";
+    if (pos === 7) styles += " rounded-bl-3xl";
+
     return (
-    <NavLink to={`/home/${friend}`}>
-        <img className={styles} src={characters[friend].img} alt={characters[friend].name}/>
-    </NavLink>
+        <NavLink
+            to={`/home/${friend}`} onClick={() => changeHero(friend)}>
+            <img className={styles} src={characters[friend].img} alt={characters[friend].name}/>
+        </NavLink>
     )
 }
 
