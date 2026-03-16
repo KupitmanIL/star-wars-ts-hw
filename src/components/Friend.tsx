7180d07335b94ec57c395764bcc0e7e76a0d4541
edward <edwardlfish@gmail.com>
2026-03-16 18:13:22 +0200

    Refactor components to update dependencies in hooks and wrap Friend images with navigation links

--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -1,4 +1,5 @@
-import {characters} from "../utils/constants.ts";
+import {characters, navItems} from "../utils/constants.ts";
+import {NavLink} from "react-router";
 
 interface Props {
     friend: string;
@@ -14,7 +15,9 @@ const Friend = ({friend, pos}: Props) => {
         styles += " rounded-bl-3xl";
     }
     return (
-        <img className={styles} src={characters[friend].img} alt={characters[friend].name}/>
+        <NavLink to={`/${navItems[0].toLowerCase()}/${friend}`}>
+            <img className={styles} src={characters[friend].img} alt={characters[friend].name}/>
+        </NavLink>
     )
 }
 
